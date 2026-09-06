4211b7483056a8bc78b0cab700a6cd03a62481f3
Mohamad Nurdiansyah <mohamad.nurdiansyah25@gmail.com>
2026-09-06 08:55:56 +0700

    fix

--- maxima-app/next.config.ts
+++ maxima-app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  allowedDevOrigins: ['maxima.bromn.biz.id'],
 };
 
 export default nextConfig;
--- maxima-app/src/proxy.ts
+++ maxima-app/src/proxy.ts
@@ -6,7 +6,7 @@ const JWT_SECRET = new TextEncoder().encode(
   process.env.JWT_SECRET || 'rahasia-banget-mvp-maxima'
 );
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // Hanya lindungi rute di bawah /admin dan /siswa
   const isAdminRoute = request.nextUrl.pathname.startsWith('/admin');
   const isSiswaRoute = request.nextUrl.pathname.startsWith('/siswa');
